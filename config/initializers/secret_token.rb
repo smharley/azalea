06bcebed9530a1de497b315e1be2105cfa642bc9
Steven Harley <smharley@gmail.com>
2013-06-28 10:59:56 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smharley::Application.config.secret_token = 'aab726e2de3ee6d2d2243d0049f1eea9e6f10f31d5d738b78bc32de03453383ef6a72421fc240b5b176a4b58823e68b072ac607fd3eac0f39a67fe5269ae3d23'
